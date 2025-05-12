0e51b278f72ff6430d40a33598e70e348f25c7ab
Arjun <peddintisuhas1114@gmail.com>
2025-05-12 19:11:02 +0530

    Basic script mode implemented

--- main.c
+++ main.c
@@ -55,42 +55,64 @@ int interactive(){
     return 0;
 }
 
-int main() {
-    interactive();
-    return 0;
-}
+// int main() {
+//     interactive();
+//     return 0;
+// }
 
 // #include <stdlib.h>
 // #include <time.h>
 // #include <ctype.h>
 // #include <math.h>
 
-// // int loader(char *);
-// int interactive();
+int loader(char *path){
+    FILE *pF = fopen(path,"r");
+    if (pF == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+    char buffer[1024];
+    while(fgets(buffer,1024,pF) != NULL){
+        // printf("%s",buffer);
+        buffer[strcspn(buffer, "\r\n")] = '\0';
+        
+        if (strlen(buffer) == 0) continue;
 
-// int main(int argc, char *argv[]){
-//     if(argc > 1){
-//          // char *path = argv[1];
-//         // loader(path);
-//     }else{
-//         interactive();
-//     }
-//     return 0;
-// }
+        allocate_tokens();
+        tokenize(buffer);
+        
+        if (debug){ printf("Tokens:\n"); print_tokens_debug();} //for debugging Tokens
 
-// // int loader(char *path){
-// //     printf("loading file: %s\n",path);
-// //     FILE *pF = fopen(path,"r");
-// //     if (pF == NULL) {
-// //         perror("Error opening file");
-// //         return 1;
-// //     }
-// //     printf("File loaded successfully!");
-// //     char buffer[1024];
-// //     while(fgets(buffer,1024,pF) != NULL){
-// //         printf("%s",buffer);
-// //     }
-// //     printf("\n");
-// //     fclose(pF);
-// //     return 0;
-// // }
+        if (strcasecmp(buffer, "exit") == 0) {
+            reset_tokens();
+            break;
+        }
+
+        if (error) goto end;
+
+        while (peek().type != TOKEN_EOF) {
+            ASTNode* root = parse_statement(NULL);
+            if (error){ ast_free(root); goto end;}
+            if (debug){ printf("\nAST:\n"); print_ast_debug(root,0,0);} //for debugging AST
+            eval(root);
+            ast_free(root);
+            if (error) goto end;
+            if (debug){ printf("\nVariables:\n"); get_variables();} //for debugging Variable Table
+        }
+        end:
+            reset_tokens();
+    }
+    printf("\n");
+    fclose(pF);
+    return 0;
+}
+
+int main(int argc, char *argv[]){
+    if(argc > 1){
+        char *path = argv[1];
+        loader(path);
+    }else{
+        interactive();
+    }
+    return 0;
+}
